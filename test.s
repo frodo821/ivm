99452f79f7891267a68b3b504c828dc188235b59
frodo821 <sakaic2003@gmail.com>
2023-10-13 18:56:46 +0900

    コードを整形した

--- test.s
+++ test.s
@@ -1,11 +1,13 @@
+_start:
   push 0
   push 100
-loop:
-    dup
-    swap2
-    add
-    swap
-    dec
-    jnz loop
+.loop:
+  dup
+  swap2
+  add
+  swap
+  dec
+  jnz .loop
+; loop ここまで
   pop
   hlt
